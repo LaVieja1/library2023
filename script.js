0aa289328b2938907f19a8c641687c0f8c282d9c
santicano <canosantiago404@gmail.com>
2023-04-13 00:04:17 -0300

    Agregado testBook

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// UI
+
+const form = document.querySelector('#form');
+
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const submitInput = document.querySelector('#submit');
+
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,6 +17,26 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(book) {
-    return myLibrary.push(book);
+submitInput.addEventListener('click', () => {
+    return addBookToLibrary();
+})
+
+function addBookToLibrary() {
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read = getReadValue();
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
+
+function getReadValue() {
+    if (form.querySelector('input[name="read"]:checked').value == 'yes') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let testBook = new Book("Harry Potter", "JK Rowling", 500, true);
+myLibrary.push(testBook);
