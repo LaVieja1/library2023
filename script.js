9a2a7a4867ebe8b162d13c272a61bb27738ef203
santicano <canosantiago404@gmail.com>
2023-04-26 00:14:00 -0300

    Añadido boton para borrar libros

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// UI
-
-const form = document.querySelector('#form');
-
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const pagesInput = document.querySelector('#pages');
-const submitInput = document.querySelector('#submit');
-
-const tableBody = document.querySelector('#book-table');
-
 // Libreria
 
 let myLibrary = [];
@@ -22,14 +11,56 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// UI
+
+const form = document.querySelector('#form');
+
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const submitInput = document.querySelector('#submit');
+
+const tableBody = document.querySelector('#book-table');
+
+const btnDelete = document.querySelector('#delete');
+
 // Events Listeners
+
+const table = document
+    .querySelector("table")
+    .addEventListener('click', (e) => {
+        const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+        if (e.target.innerHTML == 'borrar') {
+            deleteBook(currentTarget.dataset.indexNumber);
+        }
+        updateTable();
+    });
 
 submitInput.addEventListener('click', () => {
     addBookToLibrary();
     updateTable();
-})
+});
 
 // Funciones
+
+function updateTable() {
+    tableBody.innerHTML = '';
+    let i = 0;
+
+    myLibrary.forEach((book) => {
+        const htmlBook = `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td><button class="read-button">${book.read}</td>
+            <td><button id="delete" data-index-number=${i}>borrar</button></td>
+        </tr>
+        `;
+        tableBody.insertAdjacentHTML("afterbegin", htmlBook);
+        i++;
+    });
+}
 
 function addBookToLibrary() {
     let title = titleInput.value;
@@ -49,26 +80,12 @@ function getReadValue() {
     }
 }
 
-
-function updateTable() {
-    tableBody.innerHTML = '';
-
-    myLibrary.forEach((book) => {
-        const htmlBook = `
-        <tr>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.pages}</td>
-            <td><button class="read-button">${book.read}</td>
-            <td><button class="delete">borrar</button></td>
-        </tr>
-        `;
-        tableBody.insertAdjacentHTML("afterbegin", htmlBook);
-    });
+function deleteBook(currentBook) {
+    myLibrary.splice(currentBook, 1);
 }
 
 // Test
 
-let testBook = new Book("Harry Potter", "JK Rowling", 500, true);
+let testBook = new Book("Harry Potter y la Piedra filosofal", "JK Rowling", 500, "read");
 myLibrary.push(testBook);
 updateTable();
